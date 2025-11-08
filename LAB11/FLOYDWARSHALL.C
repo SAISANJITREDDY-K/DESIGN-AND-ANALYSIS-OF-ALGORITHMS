cab95d9dc6360aa4ae6e1c4e17e9ddca27f20979
K SAI SANJIT REDDY <saisanjitk@gmail.com>
2025-11-08 09:44:26 +0530

    Add Floyd-Warshall algorithm implementation
    Implement Floyd-Warshall algorithm for shortest paths

---
+++ LAB11/FLOYDWARSHALL.C
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <limits.h>
+
+#define V 4
+
+void floydWarshall(int dist[V][V]) {
+    int i, j, k;
+    for (k = 0; k < V; k++) {
+        for (i = 0; i < V; i++) {
+            for (j = 0; j < V; j++) {
+                if (dist[i][k] != INT_MAX && dist[k][j] != INT_MAX &&
+                    dist[i][k] + dist[k][j] < dist[i][j]) {
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+}
+
+void printMatrix(int dist[V][V]) {
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            if (dist[i][j] == INT_MAX)
+                printf("INF ");
+            else
+                printf("%d ", dist[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int dist[V][V] = {
+        {0, 5, INT_MAX, 10},
+        {INT_MAX, 0, 3, INT_MAX},
+        {INT_MAX, INT_MAX, 0, 1},
+        {INT_MAX, INT_MAX, INT_MAX, 0}
+    };
+
+    floydWarshall(dist);
+    printMatrix(dist);
+
+    return 0;
+}
